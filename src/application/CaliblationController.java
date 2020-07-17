6900c7b8ef12108a013be26a761f51597a76dd88
hosokawa <hosokawa@DESKTOP-RAU1CC1>
2020-07-17 19:39:00 +0900

    作りこみ中

---
+++ src/application/CaliblationController.java
@@ -0,0 +1,163 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Window;
+
+public class CaliblationController {
+
+    @FXML
+    private ResourceBundle resources;
+    @FXML
+    private URL location;
+    @FXML
+    private TextField calibWeightTX_1;
+    @FXML
+    private TextField enptyValueTX_1;
+    @FXML
+    private TextField calibValueTX_1;
+    @FXML
+    private Label GetEmptyValueInfo_1;
+    @FXML
+    private Button GetEmptyValueBT_1;
+    @FXML
+    private Button GetCalibValueBT_1;
+    @FXML
+    private Button GetEmptyValueBT_2;
+    @FXML
+    private Button GetCalibValueBT_2;
+    @FXML
+    private Label GetCalibValueInfo_1;
+    @FXML
+    private Button calibSaveBT;
+    @FXML
+    private Button calibCancelBT;
+    @FXML
+    private TextField calibWeightTX_2;
+    @FXML
+    private TextField enptyValueTX_2;
+    @FXML
+    private TextField calibValueTX_2;
+    @FXML
+    private Label GetEmptyValueInfo_2;
+    @FXML
+    private Label GetCalibValueInfo_2;
+
+    //クラス変数
+    static long[] emptyValue = new long[2];
+    static long[] calibValue = new long[2];
+    static double[] calibWeight = new double[2];
+    static HX711[] hx;//MainScreenControllerで作成されたオブジェクトの参照
+
+
+    @FXML
+    void onCalibCancelBT(ActionEvent event) {
+		Scene scene = ((Node) event.getSource()).getScene();
+		Window window = scene.getWindow();
+		window.hide();
+    }
+
+    @FXML
+    void onCalibSaveBT(ActionEvent event) {
+    	emptyValue[0] = Long.valueOf( this.enptyValueTX_1.getText() );
+    	calibValue[0] = Long.valueOf( this.calibValueTX_1.getText());
+    	calibWeight[0] = Double.valueOf( this.calibWeightTX_1.getText());
+
+    	emptyValue[1] = Long.valueOf( this.enptyValueTX_2.getText() );
+    	calibValue[1] = Long.valueOf( this.calibValueTX_2.getText());
+    	calibWeight[1] = Double.valueOf( this.calibWeightTX_2.getText());
+
+		Scene scene = ((Node) event.getSource()).getScene();
+		Window window = scene.getWindow();
+		window.hide();
+    }
+
+
+    @FXML
+    void onGetCalibValu_1(ActionEvent event) {
+    	double[]reslut = getLoadCellValue(0,this.GetCalibValueInfo_1);
+    	if( reslut != null) {
+    		Platform.runLater(() ->this.calibValueTX_1 .setText(String.valueOf(reslut[0])));
+    	}else {
+    		Platform.runLater(() ->this.calibValueTX_1.setText("Error"));
+    	}
+    }
+
+    @FXML
+    void onGetEmptyValueBT_1(ActionEvent event) {
+    	double[]reslut = getLoadCellValue( 0, this.GetEmptyValueInfo_1);
+    	if( reslut != null) {
+    		Platform.runLater(() ->this.enptyValueTX_1.setText(String.valueOf(reslut[0])));
+    	}else {
+    		Platform.runLater(() ->this.enptyValueTX_1.setText("Error"));
+    	}
+    }
+    @FXML
+    void onGetCalibValu_2(ActionEvent event) {
+    	double[]reslut = getLoadCellValue(1,this.GetCalibValueInfo_2);
+    	if( reslut != null) {
+    		Platform.runLater(() ->this.calibValueTX_2 .setText(String.valueOf(reslut[0])));
+    	}else {
+    		Platform.runLater(() ->this.calibValueTX_2.setText("Error"));
+    	}
+    }
+
+    @FXML
+    void onGetEmptyValueBT_2(ActionEvent event) {
+    	double[]reslut = getLoadCellValue( 1, this.GetEmptyValueInfo_1);
+    	if( reslut != null) {
+    		Platform.runLater(() ->this.enptyValueTX_1.setText(String.valueOf(reslut[0])));
+    	}else {
+    		Platform.runLater(() ->this.enptyValueTX_1.setText("Error"));
+    	}
+    }
+    /**
+    *
+    * @return  double[chNo] hx.value
+    */
+   public double[] getLoadCellValue(int chNo,Label dstInfoLabel){
+   	try {
+   		double[] aveValue= {0,0};
+   		final int rpeetCnt = (int)(30/0.1);
+
+   		for(int j=0;j<rpeetCnt;j++) {
+		        hx[chNo].read();
+		        System.out.println("value="+hx[chNo].value);
+		        aveValue[chNo] += hx[chNo].value;
+		        Platform.runLater(() ->dstInfoLabel.setText(String.valueOf(hx[chNo].value)));
+   		}
+
+   		double[] result= new double[hx.length];
+   		for(int i=0;i<hx.length;i++) {
+		        aveValue[i] /= rpeetCnt;
+   		}
+        return result;
+
+   	}catch(Exception e) {
+   		System.out.println(e);
+   		return null;
+
+   	}
+   }
+
+   @FXML
+   void onClose(ActionEvent event) {
+		Scene scene = ((Node) event.getSource()).getScene();
+		Window window = scene.getWindow();
+		window.hide();
+   }
+
+    @FXML
+    void initialize() {
+
+    }
+}
--- src/application/MainScreenController.java
+++ src/application/MainScreenController.java
@@ -1,5 +1,150 @@
 package application;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.wiringpi.GpioUtil;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+
+
 public class MainScreenController {
-	
+	@FXML
+	private ResourceBundle resources;
+	@FXML
+	private URL location;
+    @FXML
+    private Button getValueBT;
+    @FXML
+    private Label hxvalueLB1;
+    @FXML
+    private Label hxvalueLB2;
+    @FXML
+    private Label hxvalueLB3;
+    @FXML
+    private Label hxvalueLB4;
+    @FXML
+    private Label hxvalueLB5;
+    @FXML
+    private Label hxvalueLB6;
+    @FXML
+    private Button calibrationMenuBT;
+
+	//HX711のチャンネル数
+	final int ch_cnt =2;
+    //HX711 接続ピンリスト BCM番号で指定 「gpio readall」 で物理ピンと確認すること
+	final Pin[] pinNoDAT = {RaspiPin.GPIO_21,RaspiPin.GPIO_23};
+	final Pin[] pinNoCLK = {RaspiPin.GPIO_22,RaspiPin.GPIO_24};
+	//HX711 オブジェクト
+	GpioPinDigitalInput[] pinHXDAT = new GpioPinDigitalInput[ch_cnt];
+    GpioPinDigitalOutput[] pinHXCLK = new GpioPinDigitalOutput[ch_cnt];
+    HX711[] hx = new HX711[ch_cnt];
+
+
+    /**
+     *
+     * @return  double[ch][0] hx.value   double[ch][1] hx.weight
+     */
+    public double[][] getLoadCellValue(){
+    	try {
+    		double[] aveValue= {0,0};
+    		double[] aveWeight = {0,0};
+    		final int rpeetCnt = 2;
+
+    		for(int j=0;j<rpeetCnt;j++) {
+	    		for(int i=0;i<ch_cnt;i++) {
+			        hx[i].read();
+			        System.out.println("value="+hx[i].value);
+			        System.out.println("weight="+hx[i].weight);
+			        aveValue[i] += hx[i].value;
+			        aveWeight[i] += hx[i].weight;
+		        }
+    		}
+    		for(int i=0;i<ch_cnt;i++) {
+		        aveValue[i] /= rpeetCnt;
+		        aveWeight[i] /= rpeetCnt;
+    		}
+
+	        double[][] result= {{aveValue[0],aveWeight[0]},{aveValue[1],aveWeight[1]}};
+	        return result;
+
+    	}catch(Exception e) {
+    		System.out.println(e);
+    		double[][] result= {{-1,-1},{-1,-1}};
+    		return result;
+
+    	}
+    }
+
+    @FXML
+    void onGetValueBT(ActionEvent event) {
+    	for(int i=0;i<ch_cnt;i++) {
+	    	hx[i].emptyValue =  CaliblationController.emptyValue[i];
+	    	hx[i].calibrationValue = CaliblationController.calibValue[i];
+	    	hx[i].calibrationWeight =CaliblationController.calibWeight[i];
+    	}
+
+    	double[][] result = getLoadCellValue();
+    	Platform.runLater(() ->hxvalueLB1.setText(String.valueOf(result[0][0])));
+    	Platform.runLater(() ->hxvalueLB2.setText(String.valueOf(result[0][1])));
+
+    	Platform.runLater(() ->hxvalueLB4.setText(String.valueOf(result[1][0])));
+    	Platform.runLater(() ->hxvalueLB5.setText(String.valueOf(result[1][1])));
+
+    }
+
+    @FXML
+    void onCaliblationController(ActionEvent event) {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("caliblation.fxml"));
+		AnchorPane root = null;
+		try {
+			root = (AnchorPane) loader.load();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		Scene scene = new Scene(root);
+		Stage stage = new Stage();
+		stage.setScene(scene);
+		stage.setResizable(false);
+
+		CaliblationController.hx = this.hx;//参照を渡す
+		//設定ウィンドウを開く
+		stage.showAndWait();
+
+    }
+    @FXML
+    void initialize() {
+        GpioUtil.enableNonPrivilegedAccess();
+
+        GpioController gpio = GpioFactory.getInstance();
+
+        for(int i=0;i<ch_cnt;i++) {
+	        pinHXDAT[i] = gpio.provisionDigitalInputPin(pinNoDAT[i],
+	        		"HX_DAT"+String.valueOf(i), PinPullResistance.OFF);
+	        pinHXCLK[i] = gpio.provisionDigitalOutputPin(pinNoCLK[i],
+	        		"HX_CLK"+String.valueOf(i), PinState.LOW);
+	        hx[i] = new HX711(pinHXDAT[i], pinHXCLK[i], 128);
+        }
+
+    }
 }
+
